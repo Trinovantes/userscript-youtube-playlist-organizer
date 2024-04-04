7bd4a94615907bccf907c563f70c98962122b7df
Stephen Li <stephenli.ca@gmail.com>
2024-04-04 03:16:11 -0400

    Sync index.d.ts

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -1,15 +1,11 @@
-declare global {
-    const DEFINE: Readonly<{
-        IS_DEV: boolean
-        NAME: string
-        PRODUCT_NAME: string
-        AUTHOR: string
-        DESC: string
-        VERSION: string
-        REPO: {
-            url: string
-        }
-    }>
-}
-
-export {}
+declare const DEFINE: Readonly<{
+    IS_DEV: boolean
+    NAME: string
+    PRODUCT_NAME: string
+    AUTHOR: string
+    DESC: string
+    VERSION: string
+    REPO: {
+        url: string
+    }
+}>
